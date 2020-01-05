113d3a6754e08386e5b51a450b028b07bb5bdf60
Pooja Dwivedi <dwivedi.pooja0@gmail.com>
2020-01-05 16:04:43 -0600

    finished adding all functionality

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,6 +13,25 @@ namespace TechJobs.Controllers
             return View();
         }
 
+        public IActionResult Results(string searchType,string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs;
+            if (searchType.Equals("all")){
+                jobs = JobData.FindByValue(searchTerm);
+                ViewBag.title = "Search in all: " + searchTerm;
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType,searchTerm);
+                ViewBag.title = "Search in all:" + searchType + " " + searchTerm;
+            }
+
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.columnChoices;
+
+            return View("Index");
+
+        }
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
